8e551203767fc3bc0787827d46b40425be5e26a7
Wilson Soetomo <wilsonsoetomo@wilsons-mbp.lan>
2022-04-22 20:02:26 -0700

    base of the list items

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-var container = document.querySelector(".container");
+function makeTimeBlocks(hour, existingTodo = "") {
 
-var timeblock = document.createElement("div");
+    var currentHour = new Date().getHours();
+    var ppof = "future";
+    if(currentHour > hour) ppof = "past";
+    if(currentHour === hour) ppof = "present";
 
-timeblock.classList.add("row");
+
+  $(".container").append(
+    $(`<div class="row time-block">
+    <div class="hour col-1">${hour}:00</div>
+    <textarea name="" id="" cols="30" rows="3" class="description col-9 ${ppof}">${existingTodo}</textarea>
+    <div class="btn saveBtn col-2"></div>
+  </div>`)
+  );
+}
+
+for (var i = 9; i < 18; i++){
+    makeTimeBlocks(i);
+}
